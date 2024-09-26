917d20e5e564daa77a5eeea173aaa3843f322412
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-26 21:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	 // Convert both strings to lowercase to make the search case-insensitive
+  const s1 = str.toLowerCase();
+  const s2 = subStr.toLowerCase();
+
+  // Use indexOf to find the first occurrence of s2 in s1
+  return s1.indexOf(s2);
 }
 
 // Please do not change the code below
